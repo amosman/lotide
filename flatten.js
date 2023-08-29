a3a3ec47736a642abc9fc72a184a6fdf86816d04
Abdurahman Osman <aosman021@gmail.com>
2023-08-29 07:32:45 +0000

    flatten function

---
+++ flatten.js
@@ -0,0 +1,33 @@
+const assertArrayEqual = function(array1 , array2) {
+  let isFalse = false;
+  if (array1.length !== array2.length) {
+    console.log(`🛑🛑🛑 Assertion Failed`);
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        console.log(`🛑🛑🛑 Assertion Failed`);
+        isFalse = true;
+        break;
+      }
+    }
+    if (!isFalse) {
+      console.log(`✅✅✅ Assertion Passed`);
+    }
+  }
+};
+
+const flatten = function(array) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (!Array.isArray(array[i])) {
+      newArray.push(array[i]);
+    } else {
+      for (let j = 0; j < array[i].length; j++) {
+        newArray.push(array[i][j]);
+      }
+    }
+  }
+  return newArray;
+};
+
+assertArrayEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]); // => [1, 2, 3, 4, 5, 6]

61865f9e5a8e684cb4d58749a39e2f183d1736be
Abdurahman Osman <aosman021@gmail.com>
2023-10-22 01:21:19 +0000

    refactored middleTest.js and tailTest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,12 +1,26 @@
-const eqArrays = require('../eqArrays');
-const assertArraysEqual = require('../assertArraysEqual');
-const middle = require('../middle');
 
-assertArraysEqual(middle([1]), []), [];// =>  true 
-assertArraysEqual(middle([1, 2]), []),[]; // => true
-assertArraysEqual(middle([1, 2, 3]), [2]), [2]; // => true
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]), [3]; // => true
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]), [2, 3];/// => true
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]), [3, 4];// => true
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]), [4];// => true
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]),[4, 5];// => true
+const middle = require('../middle');
+const assert = require('chai').assert;
+describe("#tail", () => {
+  it("returns [] for [1])", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("returns [] for [1, 2]", () => {
+    assert.deepEqual(middle([1,2]), []); 
+  });
+  it("returns  [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]); 
+  })
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]); 
+  });
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]); 
+  });
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); 
+  });
+  it("returns [4] for [1, 2, 3, 4, 5, 6, 7]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]); 
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,16 @@
 const tail = require('../tail');
-const assertArraysEqual = require('../assertArraysEqual');
-
-
-assertArraysEqual(tail(["Lighthouse Labs", "Bootcamp", "Web Development"]),["Bootcamp", "Web Development"]);
-assertArraysEqual(tail(["football", "soccer", "baseball", 4 ]),["soccer", "baseball", 7]);
-assertArraysEqual(tail([1, 1, 4 , 7 ,9, "tom"]),[1, 1, 4 , 7 ,9, "Sam"]);
-assertArraysEqual(tail([]), []);
+const assert = require('chai').assert;
+describe("#tail", () => {
+  it("returns ['Bootcamp', 'Web Development'] for ['Lighthouse Labs', 'Bootcamp', 'Web Development']", () => {
+    assert.deepEqual(tail(['Lighthouse Labs', 'Bootcamp', 'Web Development']), ['Bootcamp', 'Web Development']);
+  });
+  it("returns ['soccer', 'baseball', 7] for ['football', 'soccer', 'baseball', 7 ]", () => {
+    assert.deepEqual(tail(['football', 'soccer', 'baseball', 7 ]), ['soccer', 'baseball', 7]); 
+  });
+  it("returns  [1, 4 , 7 ,9, 'tom'] for [1, 1, 4 , 7 ,9, 'tom']", () => {
+    assert.deepEqual(tail([1, 1, 4 , 7 ,9, 'tom']), [1, 4 , 7 ,9, 'tom']); 
+  })
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []); 
+  });
+});
